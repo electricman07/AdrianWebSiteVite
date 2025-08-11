02859263f7e9b4516b7e055fe9d787a669c35da8
electricman07 <glenp@telus.net>
2025-08-11 14:15:29 -0700

    removed react import from each page

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const HomeLayout = () => {
   return <div>HomeLayout</div>;
 };
--- src/pages/Qualifications.tsx
+++ src/pages/Qualifications.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Qualifications = () => {
   return <div>Qualifications</div>;
 };
