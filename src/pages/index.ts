115793606fd2938bf4948e7e8b51af5d0b0220d6
electricman07 <glenp@telus.net>
2025-08-11 14:17:50 -0700

    added error page and intex.ts

---
+++ src/pages/index.ts
@@ -0,0 +1,8 @@
+export { default as HomeLayout } from "./HomeLayout";
+export { default as About } from "./About";
+export { default as Portfolio } from "./Portfolio";
+export { default as Qualifications } from "./Qualifications";
+export { default as Contact } from "./Contact";
+export { default as Blog } from "./Blog";
+export { default as Landing } from "./Landing";
+export { default as Error } from "./Error";
